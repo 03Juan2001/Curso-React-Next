fc9f015057b0a0b1f5ae77248b59fb8a128caf61
Juan Carlos Avila <99233600+03Juan2001@users.noreply.github.com>
2025-01-29 00:53:41 +0000

    Projecto finalizado

--- vite-project/src/components/Todo.jsx
+++ vite-project/src/components/Todo.jsx
@@ -1,13 +1,13 @@
-const Todo = ({ todo }) => {
-    const { title, description, state, priority } = todo;
+const Todo = ({ todo, deleteTodo, updateTodo }) => {
+    const { title, description, state, priority, id } = todo;
     return (
         <li className="list-group-item d-flex justify-content-between align-items-start">
         <div>
             <h5 className={`${state && "text-decoration-line-through"}`}>{title}</h5>
             <p>{description}</p>
             <div className="d-flex gap-2">
-                <button className="btn btn-sm btn-danger">Eliminar</button>
-                <button className="btn btn-sm btn-info">Actualizar</button>
+                <button onClick= { () => deleteTodo(id) } className="btn btn-sm btn-danger">Eliminar</button>
+                <button onClick= { () => updateTodo(id) } className="btn btn-sm btn-warning">Actualizar</button>
             </div>
         </div>
             <span className="badge bg-primary rounden-pill">
--- vite-project/src/components/Todos.jsx
+++ vite-project/src/components/Todos.jsx
@@ -1,6 +1,6 @@
 import Todo from "./Todo";
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, deleteTodo, updateTodo }) => {
   return (
     <main>
       <h2 className="text-center my-5">Todos</h2>
@@ -9,8 +9,17 @@ const Todos = ({ todos }) => {
           <Todo
             key={todo.id}
             todo={todo}
+            deleteTodo={deleteTodo}
+            updateTodo={updateTodo}
           />
         ))}
+        {
+          todos.length === 0 && (
+            <li className = "list-group-item text-center">
+              Sin Todos
+            </li>
+          )
+        }
       </ul>
     </main>
   );
