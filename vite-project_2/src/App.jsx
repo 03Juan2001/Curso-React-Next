17aead3191cc239d2aac00399a247b1cec29b8b7
Juan Carlos Avila <juan.avila201@tectijuana.edu.mx>
2025-03-11 17:11:08 -0700

    Imgenes del proyecto

--- vite-project_2/src/App.jsx
+++ vite-project_2/src/App.jsx
@@ -1,8 +1,12 @@
 export default function App() {
   return (
-    <div className="bg-[url()] bg-cover bg-center bg-no-repeat">
+    <div className="bg-[url(./assets/IMG/bg-desktop-light.jpg)] 
+    bg-cover bg-center bg-no-repeat">
+      <header>
+        
+      </header>
       <h1 className='uppercase'>Hola Mundo Cruel!!!</h1>
-      <button>luna</button>
+      <button>Luna</button>
     </div>
   )
 }
